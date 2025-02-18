f48b99de3166fe74732925210ebce0110a8f2af3
retzam <dretzam@gmail.com>
2025-02-18 08:37:38 +0100

    update meta data

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,9 +3,9 @@ import './globals.css';
 import { Analytics } from '@vercel/analytics/react';
 
 export const metadata = {
-  title: 'Next.js App Router + NextAuth + Tailwind CSS',
+  title: 'JAMMAII',
   description:
-    'A user admin dashboard configured with Next.js, Postgres, NextAuth, Tailwind CSS, TypeScript, and Prettier.'
+    'Jammai is a premium real estate investment platform for affiliate developers'
 };
 
 export default function RootLayout({
