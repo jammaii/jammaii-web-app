94e155513f2b868b9c9834a158aeac92964876e4
retzam <dretzam@gmail.com>
2025-02-28 19:33:56 +0100

    update providers

--- components/layout/providers.tsx
+++ components/layout/providers.tsx
@@ -1,33 +1,33 @@
-"use client";
+'use client';
 
-import { AuthProvider } from "./auth-provider";
-import { cookies } from "next/headers";
-import { TRPCReactProvider } from "@/trpc/react";
-import { Analytics } from "@vercel/analytics/react";
+import { cookies } from 'next/headers';
+import { TRPCReactProvider } from '@/trpc/react';
+import { Analytics } from '@vercel/analytics/react';
+import { SessionProvider } from 'next-auth/react';
 
-export { SessionProvider } from "next-auth/react";
+export { SessionProvider } from 'next-auth/react';
 
-const ALL_APP_PROVIDERS = [AuthProvider];
-/**
- * This function takes an array of React components and combines them into one component, nesting them inside each other.
- * Creating a new component that wraps `Curr` with `Prev` if `Prev` exists, otherwise just renders `Curr` with `children`.
- *
- * @param providers An array of React components, each expecting a `children` prop.
- * @returns A new React component that combines the provided components.
- */
-const compose = (providers: React.FC<{ children: React.ReactNode }>[]) =>
-  providers.reduce((Prev, Curr) => ({ children }) => {
-    const Provider = Prev ? (
-      <Prev>
-        <Curr>{children}</Curr>
-      </Prev>
-    ) : (
-      <Curr>{children}</Curr>
-    );
-    return Provider;
-  });
+// const ALL_APP_PROVIDERS = [];
+// /**
+//  * This function takes an array of React components and combines them into one component, nesting them inside each other.
+//  * Creating a new component that wraps `Curr` with `Prev` if `Prev` exists, otherwise just renders `Curr` with `children`.
+//  *
+//  * @param providers An array of React components, each expecting a `children` prop.
+//  * @returns A new React component that combines the provided components.
+//  */
+// const compose = (providers: React.FC<{ children: React.ReactNode }>[]) =>
+//   providers.reduce((Prev, Curr) => ({ children }) => {
+//     const Provider = Prev ? (
+//       <Prev>
+//         <Curr>{children}</Curr>
+//       </Prev>
+//     ) : (
+//       <Curr>{children}</Curr>
+//     );
+//     return Provider;
+//   });
 
-const ComposedProviders = compose(ALL_APP_PROVIDERS);
+// const ComposedProviders = compose(ALL_APP_PROVIDERS);
 
 export interface ProvidersProps {
   children: React.ReactNode;
@@ -38,7 +38,8 @@ export async function Providers({ children }: ProvidersProps) {
   return (
     <TRPCReactProvider cookies={cookieData.toString()}>
       <Analytics />
-      <ComposedProviders>{children}</ComposedProviders>
+      {/** @ts-expect-error Async Server Component */}
+      <SessionProvider session={session}>{children}</SessionProvider>
     </TRPCReactProvider>
   );
 }
