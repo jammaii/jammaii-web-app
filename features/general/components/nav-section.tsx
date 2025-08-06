904cd00c1c4907e311c8369e94a4c5b91633f3b3
jammaii <andelo@jammaii.com>
2025-08-06 17:08:17 +0100

    upd

--- features/general/components/nav-section.tsx
+++ features/general/components/nav-section.tsx
@@ -13,7 +13,7 @@ const navLinks = [
   { label: 'Properties', href: '/projects' },
   { label: 'How it Works', href: '/how-it-works' },
   { label: 'About', href: '/about' },
-  { label: 'Blog', href: '#'}
+  { label: 'Blog', href: '/blog'},
   { label: 'Contact', href: '/contact' }
 ];
 
